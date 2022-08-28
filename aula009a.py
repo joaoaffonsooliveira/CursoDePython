2685bca0d86298050fb0f68d7d546209322da841
João Affonso Oliveira Alves de Araujo <affonso.oliver@gmail.com>
2022-08-28 20:18:48 -0300

    Fatiamento de strings

---
+++ aula009a.py
@@ -0,0 +1,17 @@
+# Introdução a listas e fatiamento de strings
+frase = 'Olá mundo!'
+print(frase[0])
+# Imprime o espaço em branco ' '
+print(frase[3])
+# Imprime '!'
+print(frase[9])
+# Imprime caracteres da casa 4 à 9. Ou seja: mundo
+print(frase[4:9])
+# Imprime caracteres pulando de 2 em 2
+print(frase[0:10:2])
+# Imprime do início até a casa informada
+print(frase[:5])
+# Imprime da casa informada até o máximo limite
+print(frase[2:])
+# Imprime começando pela casa 1 até o fim pulando de 3 em 3
+print(frase[1::3])
