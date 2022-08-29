72d6ea1b837b635f0f0df7b471349f6836bf13a5
João Affonso Oliveira Alves de Araujo <affonso.oliver@gmail.com>
2022-08-28 21:32:38 -0300

    Fazendo mudanças em strings

---
+++ aula009f.py
@@ -0,0 +1,7 @@
+# Como fazer uma mudança em uma string
+frase = 'Curso em Vídeo Python'
+# Imprime Curso em Vídeo Python
+print(frase)
+# Imprime Curso em Vídeo Android
+frase = frase.replace('Python', 'Android')
+print(frase)
