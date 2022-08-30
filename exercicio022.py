4dd28c6dac772635e69153316c4c9775674b5b2b
João Affonso Oliveira Alves de Araujo <affonso.oliver@gmail.com>
2022-08-29 22:32:57 -0300

    Exercicio 022

--- exercicio022.py
+++ exercicio022.py
@@ -1,5 +1,5 @@
 # Analisador de textos
-nome = input('Digite seu nome completo: ')
+nome = str(input('Digite seu nome completo: '))
 nomeSemEspacos = nome.replace(' ', '')
 nomeDividido = nome.split()
 print(f'Seu nome completo é: {nome}')
@@ -9,3 +9,16 @@ print(f'Seu nome completo em capitalize é: {nome.capitalize()}')
 print(f'Seu nome completo em title é: {nome.title()}')
 print(f'Seu nome tem {len(nomeSemEspacos)} letras')
 print(f'Seu primeiro nome possui {len(nomeDividido[0])} letras')
+
+# Outra forma de realizar o desafio:
+"""
+nome = str(input('Digite seu nome completo: ')).strip()
+print('Analisando seu nome...')
+print(f'Seu nome completo com todas as letras maiúsculas é: {nome.upper()}')
+print(f'Seu nome completo com todas as letras minúsculas é: {nome.lower()}')
+print(f'Seu nome completo em capitalize é: {nome.capitalize()}')
+print(f'Seu nome completo em title é: {nome.title()}')
+print(f'Seu nome tem {len(nome) - nome.count(" ")} letras')
+print(f'Seu primeiro nome tem {nome.find(" ")} letras')
+"""
+
