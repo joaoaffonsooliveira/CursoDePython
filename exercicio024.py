b0f68e9f2a1870dbfaceffaa1426057e558b95b1
João Affonso Oliveira Alves de Araujo <affonso.oliver@gmail.com>
2022-08-29 22:59:01 -0300

    Exercicio 024

--- exercicio024.py
+++ exercicio024.py
@@ -1,5 +1,3 @@
 # Verificando as primeiras letras de um texto
-c = input('Digite o nome de alguma cidade: ')
-# Divide o nome da cidade em uma lista de nomes
-cDividido = c.split()
-print(f'A cidade digitada começa com "Santo" em seu nome? {("Santo" in cDividido[0])}')
+cid = input('Digite o nome de alguma cidade: ').strip()
+print(f'O nome da cidade digitada pcomeça com "Santo"? {cid[:5].upper() == "SANTO"}')
