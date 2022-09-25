3f617d2fcd26f26988b75ab619001e82666ee707
João Affonso Oliveira Alves de Araujo <affonso.oliver@gmail.com>
2022-09-25 20:35:24 -0300

    exercicio003 - Adicionando cores

--- exercicio003.py
+++ exercicio003.py
@@ -2,7 +2,9 @@
 n1 = int(input('Digite um número: '))
 n2 = int(input('Digite outro número: '))
 s = n1 + n2
-print(f'A soma entre {n1} e {n2} é: {s}')
+print(f'A soma entre \033[1;31m{n1}\033[m e \033[1;31m{n2}\033[m é: \033[1;31m{s}\033[m')
 
 # Outra forma de escrever:
+'''
 print('A soma entre {} e {} é: {}'.format(n1, n2, s))
+'''
