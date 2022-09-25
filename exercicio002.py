72dfb24c50eeb4240ec1e52dfe11eb02ab91b9b7
João Affonso Oliveira Alves de Araujo <affonso.oliver@gmail.com>
2022-09-25 20:32:59 -0300

    exercicio002 - Adicionando cores

--- exercicio002.py
+++ exercicio002.py
@@ -1,11 +1,11 @@
 nome = input('Digite seu nome: ')
 
 # Primeira forma de digitar (formato que mais gosto)
-print(f'Olá, {nome} é um prazer te conhecer!')
+print(f'Olá \033[4;34m{nome}\033[m, é um prazer te conhecer!')
 
 # Segunda forma de digitar
-print('É um prazer te conhecer', nome)
+print('É um prazer te conhecer,\033[4;34m',nome,'\033[m')
 
 # Terceira forma de digitar
-print('Olá, {} é um prazer te conhecer!'.format(nome))
+print('Olá {}{}{}, é um prazer te conhecer!'.format('\033[4;34m', nome, '\033[m'))
 
