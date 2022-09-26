4d2ccd9d70f0e54bf1a16ee703ba945a06b276e1
João Affonso Oliveira Alves de Araujo <affonso.oliver@gmail.com>
2022-09-26 15:05:29 -0300

    aula012 - Condicoes aninhadas

--- aula012.py
+++ aula012.py
@@ -5,5 +5,7 @@ if nomeMaiusculo == 'GUSTAVO':
     print(f'Que nome bonito, {nome}')
 elif nomeMaiusculo == 'PEDRO':
     print(f'Que nome normal, {nome}')
+elif nomeMaiusculo in 'ANA CLÁUDIA JÉSSICA E JULIANA':
+    print('Belo nome feminino!')
 else:
     print(f'Que nome estranho, {nome}')
