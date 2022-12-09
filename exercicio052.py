55253d177a01ec4c7f6c0e18b558d6be29e19e60
João Affonso Oliveira Alves de Araujo <affonso.oliver@gmail.com>
2022-12-08 23:14:01 -0300

    exercicio052 - Números primos

--- exercicio052.py
+++ exercicio052.py
@@ -1,8 +1,15 @@
 # Números primos
 n = int(input('Digite um número: '))
+tot = 0
 for c in range(1, n + 1, 1):
     if n % c == 0:
         print('\033[33m', end=' ')
+        tot += 1
     else:
         print('\033[31m', end=' ')
     print(f'{c}', end=' ')
+print(f"\n\033[m O número {n} foi divisível {tot} vezes")
+if tot == 2:
+    print('E por isso ele é primo')
+else:
+    print('E por isso ele não é primo')
